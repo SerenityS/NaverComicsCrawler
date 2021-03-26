3c232413d32f1c496c2e1f36d0c0d931b4e0e56b
HeeRyun, Jin <jins4218@gmail.com>
2021-03-27 00:42:08 +0900

    main.py: Initialize Main Script

---
+++ main.py
@@ -0,0 +1,7 @@
+from NaverComicsCrawler import NaverComicsCrawler
+
+if __name__ == '__main__':
+    print("NaverComicsCrawler")
+    url = input("웹툰 URL을 입력하세요 : ")
+    crawler = NaverComicsCrawler(url)
+    crawler.getImage()
